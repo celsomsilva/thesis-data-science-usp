2b7e8982be42d350c23014c8c7fadc3c49623548
celsomsilva <8434441+celsomsilva@users.noreply.github.com>
2026-06-28 01:06:33 -0300

    Add code template.

---
+++ src/R/template.R
@@ -0,0 +1,15 @@
+#########################################################
+# Thesis: Nonlinear Multilevel Model for Weekly Sales Prediction
+#
+# The complete implementation will be available soon.
+# It will demonstrate the simplified workflow below:
+#
+# 1. Load the dataset (already cleaned and partially
+#    transformed in the notebook)
+# 2. Detect outliers and apply Z-score normalization
+# 3. Fit linear and nonlinear models
+# 4. Fit negative binomial model
+# 5. Fit multilevel models
+# 6. Compare model performance (LogLik, AIC, and BIC)
+#########################################################
+
